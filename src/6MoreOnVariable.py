20bdeff966388d48eb069b7df8863ba71a9e8431
Manjunath <manjunath.angadi4846@gmail.com>
2024-04-21 00:16:03 +0530

    commit no 6 for python course More On Variables

---
+++ src/6MoreOnVariable.py
@@ -0,0 +1,24 @@
+print("")
+num = 7
+print("address of num : " + str(id(num)))
+
+name = 'Manju'
+print("address of name : " + str(id(name)))
+print("")
+a = 5
+b = a
+print("address of a : " + str(id(a)))
+print('address of b : ' + str(id(b)))
+print('address of 5 : ' + str(id(5)))
+c = 5
+print('address of c : ' + str(id(c)))
+
+a = 10
+print("")
+print("address of a : " + str(id(a)))
+print('address of b : ' + str(id(b)))
+print('address of 5 : ' + str(id(5)))
+print('address of c : ' + str(id(c)))
+
+print("")
+
