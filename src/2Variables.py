52cb3249ead82d97503a95e4bf3af0dfcf323aba
Manjunath <manjunath.angadi4846@gmail.com>
2024-04-20 22:47:22 +0530

    Second commit for python course variables

---
+++ src/2Variables.py
@@ -0,0 +1,15 @@
+x = 2
+print(x + 3)
+y = 3
+print(x + y)
+x = 9
+print(x + y)
+print(x)
+print(x + 10)
+name = 'Youtube'
+print(name[0])
+print(name[0:3])
+print(name[3:])
+print(name[-1])
+print('my' + name[3:])
+print(len(name))
