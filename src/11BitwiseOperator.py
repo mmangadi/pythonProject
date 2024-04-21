6fe9eafbe93d9383c8f3803f08926205dee93118
Manjunath <manjunath.angadi4846@gmail.com>
2024-04-21 19:19:25 +0530

    commit no 8 for python course Bitwise Operator and Math functions

---
+++ src/11BitwiseOperator.py
@@ -0,0 +1,20 @@
+# Bitwise Operator
+
+# Complement [~]
+# AND [&]
+# OR [|]
+# XOR [^]
+# Left Shift [<<]
+# Right Shift [>>]
+from math import *
+
+print(~12)
+# Binary number of 12 is 00001100
+
+
+print(sqrt(25))
+print(floor(5.2))
+print(ceil(5.2))
+print(pow(3, 2))
+print(pi)
+print(e)
